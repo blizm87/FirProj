119dccb4576e3adf6c4ea35859026c5331fdc7fa
Justin Samuels <blizm87@gmail.com>
2017-06-08 09:19:05 -0700

    first commit

---
+++ FirProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirProj
+//
+//  Created by Justin Samuels on 6/8/17.
+//  Copyright © 2017 Justin Samuels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
